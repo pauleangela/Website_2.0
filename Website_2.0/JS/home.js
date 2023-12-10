43e2d1fddc6ce0cb63aeddb3ab48e15cda664a65
pauleangela <148184289+pauleangela@users.noreply.github.com>
2023-12-10 08:26:14 +0800

    Delete Website_2.0 directory

--- Website_2.0/JS/home.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("menu-panel");
-  if (x.style.display === "none") {
-    x.style.display = "flex";
-  } else {
-    x.style.display = "none";
-  }
-}
