f2312e78cf0747a61e240caf1268737f19115bd8
pauleangela <148184289+pauleangela@users.noreply.github.com>
2023-12-10 08:26:45 +0800

    Add files via upload

---
+++ JS/home.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("menu-panel");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
